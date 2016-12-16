02b5a36ba6765a8ed8e2d5d790b761d80e60995a
mcnapatm <mcnpatm@dunwoody.edu>
2016-12-16 12:13:22 -0600

    Add files via upload

--- assigment7.cs
+++ assigment7.cs
@@ -1,7 +1,7 @@
 ﻿// Hello2.cs
 using System;
 
-public class classGPA
+public class GPAClass
 {
     public static void Main()
     {
@@ -19,10 +19,27 @@ public class classGPA
 		int y = 0;
 		string[] LETTERGRADE_ARRAY = {"A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "F"};
         double[] GRADEPOINT_ARRAY = { 4.0, 3.70, 3.30, 3.00, 2.70, 2.30, 2.00, 1.70, 1.30, 1.00, 0.00 };
+        GPAC[] GPACArray; 
 
         classTotalAmount = housekeeping();
-        Console.WriteLine(classTotalAmount);
-        Console.ReadLine();
+        GPACArray = new GPAC[classTotalAmount];
+        while (x <= classTotalAmount) { 
+            if(x == 0)
+            {
+                Console.WriteLine("Please enter the name of the first class.");
+            }
+            else if(x == classTotalAmount)
+            {
+                Console.WriteLine("Please enter the name of the last class.");
+            }
+            else
+            {
+                Console.WriteLine("Please enter the name of the next class.");
+            }
+            ++x;
+        
+        }
+
 
 
     }
@@ -34,7 +51,58 @@ public class classGPA
         Console.WriteLine("It should be a number in between 1 and 6.");
         classTotalAmount = Console.ReadLine();
         int classTotalNum = Convert.ToInt32(classTotalAmount);
+        while ((classTotalNum < 1) | (classTotalNum > 6))
+        {
+            if (classTotalNum < 1) {
+                Console.WriteLine("That is too few classes. Please enter a number between 1 and 6.");
+                classTotalAmount = Console.ReadLine();
+                classTotalNum = Convert.ToInt32(classTotalAmount);
+            }
+            else if (classTotalNum > 6) {
+                Console.WriteLine("That is many few classes. Please enter a number between 1 and 6.");
+                classTotalAmount = Console.ReadLine();
+                classTotalNum = Convert.ToInt32(classTotalAmount);
+            }
+        
+            }
+
 
         return classTotalNum;
+    }
+    public class GPAC{
+
+        private string className;
+		private string letterGrade;
+		private int creditNum;
+
+        public void setClassName(string cName)
+        {
+            className = cName;
+        }
+        public void setLetterGrade(string grd)
+        {
+            letterGrade = grd;
+        }
+        public void setCreditNum(int cred)
+        {
+            creditNum = cred;
+        }
+        public string getClassName()
+        {
+
+            return className;
+        }
+        public string getLetterGrade()
+        {
+
+            return letterGrade;
+        }
+        public int getCreditNum()
+        {
+            return creditNum;
+        }
+    
+    
+    
     }
 }
